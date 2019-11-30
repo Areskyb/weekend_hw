beea90cb35778661880a068157b7d4f830e43335
Aresky Berkane <areskyberkane@gmail.com>
2019-11-30 14:17:00 +0000

    created from and imported eventBus

---
+++ src/components/FunForm.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <form class="fun-form" v-on:submit.prevent='handleForm'>
+    <textarea name="name" rows="8" cols="80" v-model="text"></textarea>
+    <input type="submit" name="" value="translate">
+  </form>
+
+</template>
+
+<script>
+import {eventBus} from "../main.js"
+
+
+export default {
+  name: "fun-form",
+  data(){
+    return{
+      text:''
+    }
+  },
+  methods: {
+    handleForm(){
+      eventBus.$emit("to-translate", this.text)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
